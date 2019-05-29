8c088a6a1ef17f1c9a6796384d74bcc0844b8316
Ashutosh Nigam <AN185105@ncr.com>
2019-05-29 16:47:44 +0530

    DRY Implementation

---
+++ DRY.Library/EmployeeProcessor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DRY.Library
+{
+    public class EmployeeProcessor
+    {
+        public string GenerateID(string firstName,string lastName)
+        {
+            return $"{firstName.Substring(0, 4)}{lastName.Substring(0, 4)}";
+        }
+    }
+}
