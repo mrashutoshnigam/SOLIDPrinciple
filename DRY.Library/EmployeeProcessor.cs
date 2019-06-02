c6fc8c8ead1d925e0be3398279dfc31e6f2f6cbb
Ashutosh Nigam <AN185105@ncr.com>
2019-06-02 12:57:54 +0530

    Dry Test Done

--- DRY.Library/EmployeeProcessor.cs
+++ DRY.Library/EmployeeProcessor.cs
@@ -10,7 +10,11 @@ namespace DRY.Library
     {
         public string GenerateID(string firstName,string lastName)
         {
-            return $"{firstName.Substring(0, 4)}{lastName.Substring(0, 4)}";
+            return $"{SubStr(firstName)}{SubStr(lastName)}";
+        }
+        private string SubStr(string str)
+        {
+            return str.Length >= 4 ? str.Substring(0, 4) : str;
         }
     }
 }
---
+++ DRY.TestProject/EmployeeTest.cs
@@ -0,0 +1,25 @@
+﻿using DRY.Library;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DRY.TestProject
+{
+    public class EmployeeTest
+    {
+        [Theory]
+        [InlineData("ashutosh", "nigam", "ashuniga")]
+        [InlineData("rajeev", "ranjan", "rajeranj")]
+        [InlineData("rit", "bhara", "ritbhar")]
+        public void GenerateEmployeeIdTest(string firstName, string lastName, string exceptedresult)
+        {
+            string empId;
+            EmployeeProcessor employeeProcessor = new EmployeeProcessor();
+            empId = employeeProcessor.GenerateID(firstName, lastName);
+            Assert.Equal(exceptedresult, empId);
+        }
+    }
+}
