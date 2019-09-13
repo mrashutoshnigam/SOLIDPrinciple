dd303c488cd3fcb88848e74e006b8b224c11ff67
Ashutosh Nigam <ashutosh.nigam@live.com>
2019-09-13 13:34:57 +0530

    Created a new project for StrategyPattern

---
+++ DesignPatterns.StrategyPattern/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DesignPatterns.StrategyPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
