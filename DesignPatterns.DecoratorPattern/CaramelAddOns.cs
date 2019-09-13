f2f634950ee726673b27f883491d541634f8112d
Ashutosh Nigam <ashutosh.nigam@live.com>
2019-09-13 22:18:45 +0530

    Decorator Pattern

---
+++ DesignPatterns.DecoratorPattern/CaramelAddOns.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.DecoratorPattern
+{
+    class CaramelAddOns : AddOnsBeverage
+    {
+        public CaramelAddOns(Beverage beverage)
+        {
+            this.beverage = beverage;
+        }
+        public override string Description()
+        {
+            return this.beverage.Description() + " + " + this.GetType().Name;
+        }
+        public override int Cost()
+        {
+            return this.beverage.Cost() + 3;
+        }
+    }
+}
---
+++ DesignPatterns.DecoratorPattern/Decaf.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.DecoratorPattern
+{
+    class Decaf : Beverage
+    {
+        public override string Description()
+        {
+            return this.GetType().Name;
+        }
+        public override int Cost()
+        {
+            return 2;
+        }
+    }
+}
