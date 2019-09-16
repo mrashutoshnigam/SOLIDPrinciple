ebea48e07368981350cb6682d108d80da1d4dcc8
Ashutosh Nigam <ashutosh.nigam@live.com>
2019-09-16 17:17:01 +0530

    Singleton Pattern

---
+++ DesignPatterns.Singleton/Singleton.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.Singleton
+{
+    class Singleton
+    {
+        private static Singleton singleTon;
+        private Singleton()
+        {
+            
+        }
+        public Singleton(int a)
+        {
+
+        }
+        public static Singleton GetInstance()
+        {
+            if (singleTon != null)
+                return singleTon;
+            else
+                return new Singleton();
+        }
+        public override string ToString()
+        {
+            return "Singleton Pattern";
+        }
+    }
+}
