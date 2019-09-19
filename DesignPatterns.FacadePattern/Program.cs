9ad3f76991fd91f3d89fe3bd2f7724b3a19ef26c
Ashutosh Nigam <ashutosh.nigam@live.com>
2019-09-19 14:21:41 +0530

    Facade Pattern

---
+++ DesignPatterns.FacadePattern/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DesignPatterns.FacadePattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Facade Pattern");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
