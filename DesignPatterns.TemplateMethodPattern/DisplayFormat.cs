35a0145be74d9c50cd75fb4be7bc16170d0b92d9
Ashutosh Nigam <ashutosh.nigam@live.com>
2019-09-20 15:13:57 +0530

    Template Method Pattern

---
+++ DesignPatterns.TemplateMethodPattern/DisplayFormat.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.TemplateMethodPattern
+{
+    abstract class DisplayFormat
+    {
+        public void Show()
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(GetData());
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(GetData());
+        }
+        protected abstract string GetData();
+
+    }
+}
---
+++ DesignPatterns.TemplateMethodPattern/NumberDisplay.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.TemplateMethodPattern
+{
+    class NumberDisplay : DisplayFormat
+    {
+        protected override string GetData()
+        {
+            return "1,2,3,4";
+        }
+    }
+}
