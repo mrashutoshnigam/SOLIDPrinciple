b401f7687d1ac587939fae9ddd46587434bffc6a
Ashutosh Nigam <ashutosh.nigam@live.com>
2019-09-25 18:42:45 +0530

    Iterator Pattern

---
+++ DesignPatterns.IteratorPattern/NotificationBar.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.IteratorPattern
+{
+    class NotificationBar
+    {
+        NotificationIterable NotificationIterable;
+        public NotificationBar(NotificationIterable notificationIterable)
+        {
+            this.NotificationIterable = notificationIterable;
+        }
+        public override string ToString()
+        {
+            var iterator = this.NotificationIterable.CreateIterator();
+            while (iterator.HasNext())
+            {
+                Notificaiton noti = iterator.Get();
+                Console.WriteLine(noti.GetNotificationContent());
+            }
+            return string.Empty;
+        }
+    }
+}
---
+++ DesignPatterns.IteratorPattern/NotificationIterator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.IteratorPattern
+{
+    class NotificationIterator : INotificationIterator
+    {
+        Notificaiton[] notificaitons;
+        int position = 0;
+        public NotificationIterator(Notificaiton[] notificaitons)
+        {
+            this.notificaitons = notificaitons;
+        }
+        public Notificaiton Get()
+        {
+            var noti = notificaitons[position];
+            position++;
+            return noti;
+
+        }
+
+        public bool HasNext()
+        {
+            if (notificaitons.Length <= position || notificaitons == null || notificaitons[position] == null)
+                return false;
+            else
+                return true;
+        }
+    }
+}
