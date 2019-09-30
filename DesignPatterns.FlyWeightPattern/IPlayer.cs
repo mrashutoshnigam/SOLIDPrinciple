d15265fcbd9f8c1337e7a72421c7a9decaa543f3
Ashutosh Nigam <ashutosh.nigam@live.com>
2019-09-30 16:39:41 +0530

    Fly Weight Pattern

---
+++ DesignPatterns.FlyWeightPattern/IPlayer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.FlyWeightPattern
+{
+    interface IPlayer
+    {
+        void AssignWeapon(string weapon);
+        void Mission();
+    }
+}
---
+++ DesignPatterns.FlyWeightPattern/Terriost.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.FlyWeightPattern
+{
+    class Terriost : IPlayer
+    {
+        readonly string TASK;
+        string weapon;
+        public Terriost()
+        {
+            TASK = "Plant Bomb";
+        }
+        public void AssignWeapon(string weapon)
+        {
+            this.weapon = weapon;
+        }
+
+        public void Mission()
+        {
+            Console.WriteLine($"Terriost with {weapon} to Complete Task: {TASK}");
+        }
+    }
+}
