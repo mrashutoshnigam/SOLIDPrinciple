6855885e05633c8e16e4e6b39c755ae992aadca2
Ashutosh Nigam <ashutosh.nigam@live.com>
2019-09-30 15:47:43 +0530

    Chain Of Responsibility Principle

---
+++ DesignPatterns.ChainOfResponsibilityPattern/IChain.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.ChainOfResponsibilityPattern
+{
+    interface IChain
+    {
+        void NextResponsibility(IChain NextChain);
+        void Handle(Number number);
+    }
+}
