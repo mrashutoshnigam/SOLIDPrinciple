1315217eafa16ecf80ae1df31ae035e4d9b7125a
Ashutosh Nigam <ashutosh.nigam@live.com>
2019-10-15 13:03:27 +0530

    Temp

---
+++ DesignPatterns.StatePattern/ClosedGateState.cs
@@ -0,0 +1,30 @@
+﻿namespace DesignPatterns.StatePattern
+{
+    internal class ClosedGateState : IGateState
+    {
+        GateContext context;
+        public ClosedGateState(GateContext context)
+        {
+            this.context = context;
+        }
+        public void GoNext()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Payment()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void PaymentFailed()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void PaymentOK()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ DesignPatterns.StatePattern/IGateState.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.StatePattern
+{
+    interface IGateState
+    {
+        void Payment();
+        void PaymentOK();
+        void PaymentFailed();
+        void GoNext();
+    }
+}
